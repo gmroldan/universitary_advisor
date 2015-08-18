7052156e1fbd7c19343d397890178e4f5957a78b
ramirokhouri <ramirokhouri@hotmail.com>
2015-08-18 01:33:43 -0300

    Refactor: Modify table Materia. Add rows: anual, 1C, 2C.

--- controllers/materia.py
+++ controllers/materia.py
@@ -2,4 +2,4 @@
 # intente algo como
 def index():
     grid = SQLFORM.smartgrid(db.materia, maxtextlength=150, paginate=100)
-    return dict(message="hello from materia.py", grid=grid)
+    return dict(grid = grid)
