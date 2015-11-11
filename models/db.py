471fc252c2846fb2a91761412631d5afcc7f5a9a
Gerardo Martin Roldan <gmroldanutn@gmail.com>
2015-11-10 23:12:33 -0300

    TECH - Create table franja_horaria

--- models/db.py
+++ models/db.py
@@ -114,14 +114,14 @@ db.define_table('estado_academico',
 db.define_table('turno',
                 Field('descripcion', requires=IS_IN_SET(['Mañana','Tarde','Noche'])),
                 Field('numero_turno', requires=IS_IN_SET(['0','1','2'])),
-                format='%(numero_turno)s'
+                format='%(descripcion)s'
                )
 
 db.define_table('modulo',
                 Field('turno', db.turno),
                 Field('hora_inicio', type='time' ,label="Hora de Inicio"),
                 Field('numero_modulo', requires=IS_IN_SET(['-1','0','1','2','3','4','5','6','7'])),
-                format='%(numero_modulo)s'
+                format='%(hora_inicio)s'
                )
 
 
@@ -131,6 +131,12 @@ db.define_table('horario_clases',
                 Field('comision', requires=IS_IN_SET(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']), label='Comisión'),
                 Field('dia', requires=IS_IN_SET(['1', '2', '3', '4', '5']), label='Día'),
                 Field('modulo','list:reference modulo', label='Módulo', widget=SQLFORM.widgets.checkboxes.widget)
+               )
+
+db.define_table('franja_horaria',
+                Field('usuario',db.auth_user, default=auth.user_id, readable=True, writable=False),
+                Field('dia', requires=IS_IN_SET(['1', '2', '3', '4', '5']), label='Día', unique=True),
+                Field('horario', 'list:reference modulo', label='Horario', widget=SQLFORM.widgets.checkboxes.widget)
                )
 
 for table in db.tables():
