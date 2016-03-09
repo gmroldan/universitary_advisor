ace1abf5f4d68ac69b0a7e6a1fad313e60dad4f2
Gerardo Martin Roldan <gmroldanutn@gmail.com>
2016-03-09 20:33:16 -0300

    Actualizando menú y pantalla principal.

--- models/menu.py
+++ models/menu.py
@@ -4,5 +4,8 @@ response.meta.author = '%(author)s <%(author_email)s>' % settings
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = [
-(T('Index'),URL('default','index')==URL(),URL('default','index'),[]),
+(T('Inicio'),URL('default','index')==URL(),URL('default','index'),[]),
+    (T('Estado Académico'),URL('estado_academico','index')==URL(),URL('estado_academico','index'),[]),
+    (T('Disponibilidad Horaria'),URL('franja_horaria','index')==URL(),URL('franja_horaria','index'),[]),
+    (T('Optimización'),URL('asistente','index')==URL(),URL('asistente','index'),[]),
 ]
